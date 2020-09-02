62e5cfc65128e4624ecb7fd4992a2e0a8a105185
Jon Willis <jondwillis@gmail.com>
2020-09-01 20:02:43 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/realm/realm-cocoa.git", from: "3.18.0"),
-        .package(url: "https://github.com/wickwirew/Runtime.git", from: "2.1.0")
+        .package(url: "https://github.com/arturdev/RuntimeNew.git", from: "2.1.0")
     ],
     targets: [
         .target(
